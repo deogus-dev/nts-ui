9db5dea199383e87ebb6987b73bade20461e90ec
Deahyeon Jang <zdh8456@gmail.com>
2023-06-13 17:11:04 +0900

    excel export 기능 추가(샘플)

--- src/router/exam.js
+++ src/router/exam.js
@@ -1,9 +1,19 @@
 import helloWorld from '~/components/HelloWorld.vue'
+import exportExcel from '~/views/exportExcel.vue'
+import exportExcelLib from '~/views/exportExcelLib.vue'
 
 const routes = [
   {
     path: '/exam',
     component: helloWorld
+  },
+  {
+    path: '/exportExcel',
+    component: exportExcel
+  },
+  {
+    path: '/exportExcelLib',
+    component: exportExcelLib
   }
 ]
 
