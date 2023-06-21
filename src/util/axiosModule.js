ac84266b46b3f6d1bcdd1e959e9f839ced1ca4db
Deahyeon Jang <zdh8456@gmail.com>
2023-06-21 18:04:26 +0900

    reissue 등 변경사항 커밋

--- src/util/axiosModule.js
+++ src/util/axiosModule.js
@@ -1,5 +1,9 @@
 import axios from 'axios'
+import _ from 'lodash'
+import { useRouter } from 'vue-router'
 import { useAuthStore } from '~/stores/auth'
+
+const router = useRouter()
 
 const instance = axios.create({
   baseURL: '/api',
@@ -14,11 +18,18 @@ const axiosModule = {
   instance,
   async api(options) {
     // API 요청 전처리(토큰)
-    // code here
 
-    let settings = options
-    settings.headers = {}
-    settings.headers.Authorization = 'Bearer ' + useAuthStore().accessToken
+    // let settings
+
+    // settings.headers = {}
+    // settings.headers.Authorization = 'Bearer ' + useAuthStore().accessToken
+
+    // if(isLogin)
+    _.merge(options, {
+      headers: {
+        Authorization: 'Bearer ' + useAuthStore().accessToken
+      }
+    })
 
     // API call
     try {
@@ -26,17 +37,29 @@ const axiosModule = {
       response = await instance(options)
       return Promise.resolve(response.data)
     } catch (error) {
-      console.log('%c[error]', 'color:red', error.response.data.status, error.response.data.message)
+      console.log('%c[error]', 'color:red', error, error.response.status, error.response.statusText)
 
-      const status = error.response.data.status
+      const status = error.response.status
       if (status === 401) {
         // at만료, 토큰 재발급 요청
         console.log('%c[token expired]', 'color: red', error)
+
+        axiosModule
+          .api({
+            url: '/reissue',
+            data: {
+              refreshToken: useAuthStore().refreshToken
+            }
+          })
+          .then((res) => {
+            authStore.saveToken(res)
+            return axiosModule.api(options)
+          })
       } else {
         console.log('[unknown error]', error)
       }
-    } finally {
-      console.log('end api')
+
+      return Promise.reject(error)
     }
   }
 }
