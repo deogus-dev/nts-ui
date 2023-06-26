f91f4dcb684330693a1b3ada06252e9b99823b8b
daehyeon jang <deogus.dev@gmail.com>
2023-06-26 23:25:24 +0900

    [Modify] validation 정규식 형식 체크

--- src/util/validation.js
+++ src/util/validation.js
@@ -1,26 +1,39 @@
 import globalConstant from '~/util/globalConstant'
 
+var regx = {
+  email:
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  password: /^(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*])[a-z\d!@#$%^&*]+$/,
+  authCode: /^\d{6}$/
+}
+
 const valid = (type, target) => {
   if (!target) {
     alert(globalConstant[type] + '을 입력해주세요')
     return false
   }
 
-  switch (type) {
-    case 'email':
-      if (target.indexOf('@') == -1) {
-        alert('이메일 형식이 아닙니다.')
-        return false
-      }
-
-    case 'password':
-      //비밀번호 형식
-      return true
-
-    case 'authCode':
-      //인증번호 6자리 체크
-      return true
+  if (!regx[type].test(target)) {
+    alert(globalConstant[type] + ' 형식이 올바르지 않습니다!')
+    return false
   }
+
+  //   switch (type) {
+  //     case 'email':
+  //       if (!regx.email.test(target)) {
+  //         alert('이메일 형식이 아닙니다!')
+  //         return false
+  //       }
+
+  //     case 'password':
+  //       //비밀번호 형식
+  //       return true
+
+  //     case 'authCode':
+  //         if(!regx.authCode.test(target))
+  //       //인증번호 6자리 체크
+  //       return true
+  //   }
 
   return true
 }
