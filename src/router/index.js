04be91079430de9c02d13974f416670a781fc03f
Deahyeon Jang <zdh8456@gmail.com>
2023-07-03 17:36:22 +0900

    [Modify] actions를 위한 baseurl 설정 변경

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { useAuthStore } from '../stores/auth'
 
-import notFound from '~/views/404.vue'
+import SingleLayout from '~/layout/SingleLayout.vue'
 
 import auth from '~/router/auth'
 import cmn from '~/router/cmn'
@@ -11,17 +11,17 @@ const routes = [...auth, ...cmn, ...exam]
 
 routes.push({
   path: '/:pathMatch(.*)*',
-  component: notFound
-  // children: [
-  //   {
-  //     path: '',
-  //     component: () => import('~/views/404.vue')
-  //   }
-  // ]
+  component: SingleLayout,
+  children: [
+    {
+      path: '',
+      component: () => import('~/views/404.vue')
+    }
+  ]
 })
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/nts-ui/'),
   routes
 })
 
