04b05bd0e11ea8fd9f0e865dcf7c2e438235cb36
daehyeon jang <deogus.dev@gmail.com>
2023-07-09 14:58:27 +0900

    api 주소 표시

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <script setup>
+import { computed } from 'vue'
 import loading from '~/components/loading.vue'
 import { useCmnStore } from '~/stores/cmn'
+
+const apiUrl = computed(() => {
+  return import.meta.env.VITE_APP_API_URL
+})
 </script>
 <template>
+  {{ apiUrl }}
   <loading v-if="useCmnStore().isLoading" />
   <router-view v-slot="{ Component, route }">
     <transition name="slide-fade" mode="out-in">
